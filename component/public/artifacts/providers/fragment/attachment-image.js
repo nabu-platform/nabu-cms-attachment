9de7de601ffc939e61677d9ceacb8a4a3daacde6
Alexander Verbruggen <alex@nabu.be>
2019-02-02 15:21:21 +0100

    updated

--- component/public/artifacts/providers/fragment/attachment-image.js
+++ component/public/artifacts/providers/fragment/attachment-image.js
@@ -3,6 +3,7 @@ Vue.component("page-field-fragment-attachment-image-configure", {
 		+ "		<n-page-mapper :to=\"['nodeId']\" :from='$services.page.getAvailableParameters(page, cell)'  v-model='fragment'/>"
 		+ "		<n-form-combo v-model='fragment.attachmentId' label='Attachment Id' :items='keys'/>"
 		+ "		<n-form-text v-model='fragment.height' label='Height'/>"
+		+ "		<n-form-text v-model='fragment.width' label='Width'/>"
 		+ "		<n-form-combo v-model='fragment.size' label='Sizing' :nillable='false' :items=\"['cover', 'contain']\"/>"
 		+ "</n-form-section>",
 	props: {
@@ -41,7 +42,7 @@ Vue.component("page-field-fragment-attachment-image-configure", {
 });
 
 Vue.component("page-field-fragment-attachment-image", {
-	template: "<div class='attachment-image' :style=\"{'background-image': 'url(' + $services.attachment.url(getField(fragment.nodeId), data[fragment.attachmentId]) + ')', 'height': fragment.height ? fragment.height : 'inherit', 'background-size': fragment.size}\"/>",
+	template: "<div class='attachment-image' :style=\"{'background-image': 'url(' + $services.attachment.url(getField(fragment.nodeId), attachmentId) + ')', 'height': fragment.height ? fragment.height : 'inherit', 'width': fragment.width ? fragment.width : 'inherit', 'background-size': fragment.size}\"/>",
 	props: {
 		cell: {
 			type: Object,
@@ -59,6 +60,17 @@ Vue.component("page-field-fragment-attachment-image", {
 			type: Object,
 			required: true
 		}
+	},
+	computed: {
+		attachmentId: function() {
+			if (this.data) {
+				return this.$services.page.getValue(this.data, this.fragment.attachmentId);
+			}
+			else {
+				var pageInstance = this.$services.page.getPageInstance(this.page, this);
+				return this.$services.page.getBindingValue(pageInstance, this.fragment.attachmentId, this);
+			}
+		}	
 	},
 	methods: {
 		getField: function(field) {
